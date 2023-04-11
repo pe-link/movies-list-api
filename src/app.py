4ab0580a6d57e6fa5019c00c3d81549ecd1ca7df
tqi_plink <tqi_plink@uolinc.com>
2023-04-11 11:29:37 -0300

    create business rule to find interval winners

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, Response
 from typing import Any
 from config import config
 from database import SQLiteDataBase
 from sqlalchemy.orm import Session
+from services import ProducersService
 
 
 class App(Flask):
@@ -14,8 +15,19 @@ class App(Flask):
             cls._instance._database = SQLiteDataBase(csv_file=config.MOVIES_CSV_FILE)
         return cls._instance
 
+    @property
+    def db_session(self: 'App') -> Session:
+        return self._database.session
+
 
 app = App(__name__)
+
+
+@app.route('/producers/range-winners', methods=['GET'])
+def get_winners_interval() -> Response:
+    intervals_ranges = ProducersService.find_interval_range(app.db_session)
+    response = intervals_ranges.to_dict()
+    return jsonify(response)
 
 
 if __name__ == '__main__':
