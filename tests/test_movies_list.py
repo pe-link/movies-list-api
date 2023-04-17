8cd87e4a2c1b5b54d8a796e53e8fd360239faffb
tqi_plink <tqi_plink@uolinc.com>
2023-04-17 15:56:30 -0300

    last tests solved

--- tests/test_movies_list.py
+++ tests/test_movies_list.py
@@ -69,15 +69,15 @@ def test_insert_csv_database(database):
         assert len(movies) > 0
 
 
-def test_find_interval_range_max_and_min_intervals(memory_db):
+def test_find_interval_range_max_and_min_intervals_in_memory_db(memory_db):
     movie1 = Movie(title='Movie 1', year=2000, studios='Studio 1', winner=True)
-    movie2 = Movie(title='Movie 2', year=2005, studios='Studio 2', winner=True)
+    movie2 = Movie(title='Movie 2', year=2001, studios='Studio 2', winner=True)
     movie3 = Movie(title='Movie 3', year=2010, studios='Studio 3', winner=True)
-    movie4 = Movie(title='Movie 4', year=2015, studios='Studio 4', winner=True)
+    movie4 = Movie(title='Movie 4', year=2017, studios='Studio 4', winner=True)
     movie5 = Movie(title='Movie 5', year=2020, studios='Studio 5', winner=True)
-    producer1 = Producer(name='Producer 1', movies=[movie1, movie2, movie3])
+    producer1 = Producer(name='Producer 1', movies=[movie1, movie2])
     producer2 = Producer(name='Producer 2', movies=[movie3, movie4])
-    producer3 = Producer(name='Producer 3', movies=[movie4, movie5])
+    producer3 = Producer(name='Producer 3', movies=[movie2, movie5])
     memory_db.add_all([movie1, movie2, movie3, movie4, movie5, producer1, producer2, producer3])
     memory_db.commit()
     win_interval_times = ProducersService.find_interval_range(memory_db)
@@ -87,18 +87,14 @@ def test_find_interval_range_max_and_min_intervals(memory_db):
     """
 
     # MIN INTERVALS
-    assert len(win_interval_times.min_intervals) == 4
+    assert len(win_interval_times.min_intervals) == 1
     assert sorted(win_interval_times.min_intervals, key=lambda x: x.producer_name)[0].producer_name == 'Producer 1'
-    assert sorted(win_interval_times.min_intervals, key=lambda x: x.producer_name)[0].interval == 5
-    assert sorted(win_interval_times.min_intervals, key=lambda x: x.producer_name)[2].producer_name == 'Producer 2'
-    assert sorted(win_interval_times.min_intervals, key=lambda x: x.producer_name)[2].interval == 5
+    assert sorted(win_interval_times.min_intervals, key=lambda x: x.producer_name)[0].interval == 1
 
     # MAX INTERVALS
-    assert len(win_interval_times.max_intervals) == 4
-    assert sorted(win_interval_times.max_intervals, key=lambda x: x.producer_name)[0].producer_name == 'Producer 1'
-    assert sorted(win_interval_times.max_intervals, key=lambda x: x.producer_name)[0].interval == 5
-    assert sorted(win_interval_times.max_intervals, key=lambda x: x.producer_name)[2].producer_name == 'Producer 2'
-    assert sorted(win_interval_times.max_intervals, key=lambda x: x.producer_name)[2].interval == 5
+    assert len(win_interval_times.max_intervals) == 1
+    assert sorted(win_interval_times.max_intervals, key=lambda x: x.producer_name)[0].producer_name == 'Producer 3'
+    assert sorted(win_interval_times.max_intervals, key=lambda x: x.producer_name)[0].interval == 19
 
 
 def test_mock_consecutive_wins_min_and_max_intervals():
@@ -108,6 +104,7 @@ def test_mock_consecutive_wins_min_and_max_intervals():
     movie_1_mock = Mock(spec=Movie)
     movie_2_mock = Mock(spec=Movie)
     movie_3_mock = Mock(spec=Movie)
+    movie_4_mock = Mock(spec=Movie)
 
     producer_1_mock.name = 'Producer 1'
     producer_2_mock.name = 'Producer 2'
@@ -117,8 +114,10 @@ def test_mock_consecutive_wins_min_and_max_intervals():
     movie_2_mock.winner = True
     movie_3_mock.year = 2010
     movie_3_mock.winner = True
+    movie_4_mock.year = 2020
+    movie_4_mock.winner = True
     producer_1_mock.movies = [movie_1_mock, movie_2_mock]
-    producer_2_mock.movies = [movie_3_mock]
+    producer_2_mock.movies = [movie_3_mock, movie_4_mock]
     db_session_mock.query.return_value.join.return_value.filter.return_value.all.return_value = [producer_1_mock, producer_2_mock]
 
     result = ProducersService.find_interval_range(db_session_mock)
@@ -132,7 +131,7 @@ def test_mock_consecutive_wins_min_and_max_intervals():
 
     # MAX INTERVALS
     assert len(result.max_intervals) == 1
-    assert result.max_intervals[0].producer_name == 'Producer 1'
-    assert result.max_intervals[0].interval == 5
-    assert result.max_intervals[0].previous_win == 2000
-    assert result.max_intervals[0].following_win == 2005
+    assert result.max_intervals[0].producer_name == 'Producer 2'
+    assert result.max_intervals[0].interval == 10
+    assert result.max_intervals[0].previous_win == 2010
+    assert result.max_intervals[0].following_win == 2020
